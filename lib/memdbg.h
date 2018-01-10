d668889403c3ffd5141623e3fde179d2f3c80236
Anton Kachanov <kachanov@i423team.ru>
2018-01-10 12:56:37 +0300

    added debug statuses

--- lib/memdbg.h
+++ lib/memdbg.h
@@ -27,16 +27,34 @@ void* _dbg_memcpy(
     _In_ char*        srcFile,
     _In_ unsigned int fileLine);
 
-#define dbg_calloc(_num, _size)                        /* calloc */ \
+errno_t _dbg_memcpy_s(
+    _In_ void*        destptr,
+    _In_ size_t       destSize,
+    _In_ const void*  srcptr,
+    _In_ size_t       count,
+    _In_ char*        srcFile,
+    _In_ unsigned int fileLine);
+
+
+#define dbg_calloc(_num, _size)                               /* calloc   */ \
     _dbg_calloc((_num), (_size), __FILE__, __LINE__)
 
-#define dbg_malloc(_size)                              /* malloc */ \
+#define dbg_malloc(_size)                                     /* malloc   */ \
     _dbg_malloc((_size), __FILE__, __LINE__)
 
-#define dbg_free(_buf)                                 /* free   */ \
+#define dbg_free(_buf)                                        /* free     */ \
     _dbg_free((_buf), __FILE__, __LINE__)
 
-#define dbg_memcpy(_destptr, _srcptr, _num)            /* memcpy */ \
+#define dbg_memcpy(_destptr, _srcptr, _num)                   /* memcpy   */ \
     _dbg_memcpy((_destptr), (_srcptr), (_num), __FILE__, __LINE__)
 
+#define dbg_memcpy_s(_destptr, _destSize, _srcptr, _count)    /* memcpy_s */ \
+    _dbg_memcpy_s((_destptr), (_destSize), (_srcptr), (_count), __FILE__, __LINE__)
+
+
+
+
+#define DBGSTATUS_BUFFER_OVERFLOW           ((errno_t)0x80000005L)
+#define DBGSTATUS_MEMORY_NOT_ALLOCATED      ((errno_t)0xC00000A0L)
+#define DBGSTATUS_SUCCESS                   ((errno_t)0x00000000L)
 #endif // memdbg_h__
